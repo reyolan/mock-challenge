62a68d117804e2d06b1bfe75de609a646302f4ea
reyolan <reynardolan@gmail.com>
2022-08-30 21:13:38 +0800

    add test

---
+++ lib/label_maker.rb
@@ -0,0 +1,28 @@
+class LabelMaker
+  NUMBER_OF_ALPHABET = 26
+  ALPHABET_ARRAY = ('A'..'Z').to_a
+
+  def initialize; end
+
+  def get_equivalent_sequence(integer)
+    return nil unless integer.is_a?(Integer) && integer.positive?
+
+    compute_equivalent_sequence(integer)
+  end
+
+  private
+
+  def compute_equivalent_sequence(integer)
+    return '' if integer.zero?
+
+    quotient = integer.to_f / NUMBER_OF_ALPHABET
+
+    return ALPHABET_ARRAY[integer - 2] if (integer % 1).zero? && integer.is_a?(Float)
+    return ALPHABET_ARRAY[integer - 1] if integer < 27
+
+    gap_from_alphabet = NUMBER_OF_ALPHABET * quotient.to_i
+    difference = integer - gap_from_alphabet - 1
+
+    compute_equivalent_sequence(quotient) + (ALPHABET_ARRAY[difference.to_i])
+  end
+end
---
+++ spec/label_maker_spec.rb
@@ -0,0 +1,35 @@
+require 'label_maker'
+
+RSpec.describe LabelMaker, '#get_equivalent_sequence' do
+  let(:label_maker) { LabelMaker.new }
+
+  context 'with a non-integer input' do
+    it 'returns nil' do
+      result = label_maker.get_equivalent_sequence('abc')
+      expect(result).to eq nil
+    end
+  end
+
+  context 'with an integer less than zero' do
+    it 'returns nil' do
+      result = label_maker.get_equivalent_sequence(-1)
+      expect(result).to eq nil
+    end
+  end
+
+  context 'with an integer input greater than zero' do
+    context 'and when integer is less than alphabet count' do
+      it 'returns corresponding string' do
+        result = label_maker.get_equivalent_sequence(1)
+        expect(result).to eq 'A'
+      end
+    end
+
+    context 'and when integer is greater than alphabet count' do
+      it 'returns an appended string' do
+        result = label_maker.get_equivalent_sequence(100)
+        expect(result).to eq 'CV'
+      end
+    end
+  end
+end
