f4072a46fdbd7576063b68a2a3a6520e95611a35
reyolan <reynardolan@gmail.com>
2022-08-30 21:46:06 +0800

    add eigenclass

--- lib/label_maker.rb
+++ lib/label_maker.rb
@@ -2,27 +2,28 @@ class LabelMaker
   NUMBER_OF_ALPHABET = 26
   ALPHABET_ARRAY = ('A'..'Z').to_a
 
-  def initialize; end
+  class << self
 
-  def get_equivalent_sequence(integer)
-    return nil unless integer.is_a?(Integer) && integer.positive?
+    def get_equivalent_sequence(integer)
+      return nil unless integer.is_a?(Integer) && integer.positive?
 
-    compute_equivalent_sequence(integer)
-  end
+      compute_equivalent_sequence(integer)
+    end
 
-  private
+    private
 
-  def compute_equivalent_sequence(integer)
-    return '' if integer.zero?
+    def compute_equivalent_sequence(integer)
+      return '' if integer.zero?
 
-    quotient = integer.to_f / NUMBER_OF_ALPHABET
+      quotient = integer.to_f / NUMBER_OF_ALPHABET
 
-    return ALPHABET_ARRAY[integer - 2] if (integer % 1).zero? && integer.is_a?(Float)
-    return ALPHABET_ARRAY[integer - 1] if integer < 27
+      return ALPHABET_ARRAY[integer - 2] if (integer % 1).zero? && integer.is_a?(Float)
+      return ALPHABET_ARRAY[integer - 1] if integer < 27
 
-    gap_from_alphabet = NUMBER_OF_ALPHABET * quotient.to_i
-    difference = integer - gap_from_alphabet - 1
+      gap_from_alphabet = NUMBER_OF_ALPHABET * quotient.to_i
+      difference = integer - gap_from_alphabet - 1
 
-    compute_equivalent_sequence(quotient) + (ALPHABET_ARRAY[difference.to_i])
+      compute_equivalent_sequence(quotient) + (ALPHABET_ARRAY[difference.to_i])
+    end
   end
 end
--- spec/label_maker_spec.rb
+++ spec/label_maker_spec.rb
@@ -1,18 +1,16 @@
 require 'label_maker'
 
 RSpec.describe LabelMaker, '#get_equivalent_sequence' do
-  let(:label_maker) { LabelMaker.new }
-
   context 'with a non-integer input' do
     it 'returns nil' do
-      result = label_maker.get_equivalent_sequence('abc')
+      result = LabelMaker.get_equivalent_sequence('abc')
       expect(result).to eq nil
     end
   end
 
   context 'with an integer less than zero' do
     it 'returns nil' do
-      result = label_maker.get_equivalent_sequence(-1)
+      result = LabelMaker.get_equivalent_sequence(-1)
       expect(result).to eq nil
     end
   end
@@ -20,14 +18,14 @@ RSpec.describe LabelMaker, '#get_equivalent_sequence' do
   context 'with an integer input greater than zero' do
     context 'and when integer is less than alphabet count' do
       it 'returns corresponding string' do
-        result = label_maker.get_equivalent_sequence(1)
+        result = LabelMaker.get_equivalent_sequence(1)
         expect(result).to eq 'A'
       end
     end
 
     context 'and when integer is greater than alphabet count' do
       it 'returns an appended string' do
-        result = label_maker.get_equivalent_sequence(100)
+        result = LabelMaker.get_equivalent_sequence(100)
         expect(result).to eq 'CV'
       end
     end
